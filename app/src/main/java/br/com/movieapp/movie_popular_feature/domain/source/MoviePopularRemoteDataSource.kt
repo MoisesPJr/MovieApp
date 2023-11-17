6d74de9524565a1d1e634b5e7cd5db921e9d55e0
MoisesPJr <moises.pereira2@fatec.sp.gov.br>
2023-11-17 17:38:26 -0300

    created Dependency injection using Hilt

---
+++ app/src/main/java/br/com/movieapp/movie_popular_feature/domain/source/MoviePopularRemoteDataSource.kt
@@ -0,0 +1,12 @@
+package br.com.movieapp.movie_popular_feature.domain.source
+
+import br.com.movieapp.core.paging.MoviePagingSource
+import br.com.movieapp.core.remote.response.MovieResponse
+
+interface MoviePopularRemoteDataSource {
+
+    fun getPopularMoviePagingSource():MoviePagingSource
+
+    suspend fun getPopularMovies(page: Int): MovieResponse
+
+}
