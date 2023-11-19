8dee76ed9741cdbdb4d6d68e0440f6bb79cba614
MoisesPJr <moises.pereira2@fatec.sp.gov.br>
2023-11-19 18:47:42 -0300

    Finished Splash screen

--- app/src/main/java/br/com/movieapp/core/presentation/MainActivity.kt
+++ app/src/main/java/br/com/movieapp/core/presentation/MainActivity.kt
@@ -3,16 +3,8 @@ package br.com.movieapp.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import br.com.movieapp.BuildConfig
-import br.com.movieapp.core.presentation.navigation.BottomNavigationBar
 import br.com.movieapp.core.presentation.navigation.MainScreen
 import br.com.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,10 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    val apiKey = BuildConfig.API_KEY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MovieAppTheme {
                 MainScreen(navController = rememberNavController())
